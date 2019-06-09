50bb82052206df62212f83c157da5edf3b77472e
17729389320 <17729389320@163.com>
2019-06-09 15:16:13 +0800

    client和server的第一个版本

---
+++ src/main/java/com/zzy/netty/Client.java
@@ -0,0 +1,89 @@
+package com.zzy.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.ReferenceCountUtil;
+
+public class Client {
+
+	public static void main(String[] args) throws Exception {
+		//线程池
+		EventLoopGroup group = new NioEventLoopGroup(1);
+		Bootstrap b = new Bootstrap();
+		try {			
+			ChannelFuture f = 
+					b.group(group)
+				.channel(NioSocketChannel.class)
+				.handler(new ClientChannelInitializer())
+				.connect("localhost", 8888)
+				;
+						
+			f.addListener(new ChannelFutureListener() {
+				@Override
+				public void operationComplete(ChannelFuture future) throws Exception {
+					if(!future.isSuccess()) {
+						System.out.println("not connected!");
+					} else {
+						System.out.println("connected!");
+					}
+				}
+			});
+			
+			f.sync();
+			
+			System.out.println("...");
+			
+			
+			f.channel().closeFuture().sync();
+		} finally {
+			group.shutdownGracefully();
+		}
+	}
+}
+
+class ClientChannelInitializer extends ChannelInitializer<SocketChannel> {
+
+	@Override
+	protected void initChannel(SocketChannel ch) throws Exception {
+		ch.pipeline().addLast(new ClientHandler());
+	}
+	
+}
+
+class ClientHandler extends ChannelInboundHandlerAdapter {
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		ByteBuf buf = null;
+		try {
+			buf = (ByteBuf)msg;	
+			byte[] bytes = new byte[buf.readableBytes()];
+			buf.getBytes(buf.readerIndex(), bytes);
+			System.out.println(new String(bytes));
+			//System.out.println(buf);
+			//System.out.println(buf.refCnt());
+		} finally {
+			if(buf != null) ReferenceCountUtil.release(buf);
+			//System.out.println("引用个数:"+buf.refCnt());
+		}
+	}
+
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		//channle 第一次连上可用，写出一个字符串 Direct Memory 
+		//普通的ByteBuffer中，网络数据首先写给操作系统，java虚拟机使用时候，需要先copy到虚拟机内存中，而 ByteBuf从虚拟机直接访问操作操作系统内存 ，效率高；
+		ByteBuf buf = Unpooled.copiedBuffer("hello".getBytes());
+		ctx.writeAndFlush(buf);//自动释放
+	}
+	
+}
---
+++ src/main/java/com/zzy/netty/Server.java
@@ -0,0 +1,122 @@
+package com.zzy.netty;
+
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.GlobalEventExecutor;
+/*
+ * Channel，表示一个连接，可以理解为每一个请求，就是一个Channel。
+   ChannelHandler，核心处理业务就在这里，用于处理业务请求。
+   ChannelHandlerContext，用于传输业务数据。
+   ChannelPipeline，用于保存处理过程需要用到的ChannelHandler和ChannelHandlerContext。
+   	注意：netty中的方法绝大部分是异步的；
+ */
+
+public class Server {
+	//处理通道组的所有的事件，便利
+	//public static ChannelGroup clients = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+	static final int PORT = 8888;
+	public static void main(String[] args) throws Exception {
+		EventLoopGroup bossGroup = new NioEventLoopGroup(5);//管理请求的线程 ，用于处理服务器端接收客户端连接  
+		EventLoopGroup workerGroup = new NioEventLoopGroup(2);//进行网络通信（读写）  
+		
+		try {
+			//创建一个ServerBootstrap对象，配置Netty的一系列参数，例如接受传出数据的缓存大小等。
+			ServerBootstrap b = new ServerBootstrap();
+			 //绑定两个线程组 
+			ChannelFuture f = b.group(bossGroup, workerGroup)
+				.channel(NioServerSocketChannel.class)//指定NIO的模式
+				.childHandler(new ChannelInitializer<SocketChannel>() {//创建一个用于实际处理数据的类ChannelInitializer，进行初始化的准备工作，比如设置接受传出数据的字符集、格式以及实际处理数据的接口。
+					@Override
+					protected void initChannel(SocketChannel ch) throws Exception {
+						ChannelPipeline pl = ch.pipeline();//管道，添加自定义的处理器
+						//处理请求的handler
+						pl.addLast(new ServerChildHandler());
+					}
+				})
+				.bind(PORT)//绑定8888端口
+				.sync();
+			
+			if (f.isSuccess()) {
+				System.out.println("Server服务器已启动，监听端口：" + PORT);
+			}
+			
+			f.channel().closeFuture().sync(); //调用close()方法是调用该方法->ChannelFuture
+		} finally {
+			workerGroup.shutdownGracefully();
+			bossGroup.shutdownGracefully();
+		}
+		
+	}
+}
+
+class ServerChildHandler extends ChannelInboundHandlerAdapter { //SimpleChannleInboundHandler Codec
+	//客户端连接成功，初始化的时候调用此方法
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("channelActive()");
+		//添加至通道组，
+		//Server.clients.add(ctx.channel());
+	}
+	//客户端写数据时，调用该方法
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		
+		ByteBuf buf = null;
+		try {
+			buf = (ByteBuf)msg;
+			
+			byte[] bytes = new byte[buf.readableBytes()];
+			buf.getBytes(buf.readerIndex(), bytes);
+			System.out.println(new String(bytes));
+			
+			ctx.writeAndFlush(msg);
+//			
+//			System.out.println(buf);
+//			System.out.println(buf.refCnt());
+		} finally {
+			//if(buf != null && buf) ReferenceCountUtil.release(buf);
+			//System.out.println(buf.refCnt());
+		}
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		cause.printStackTrace();
+		ctx.close();
+	}
+	
+	
+}
+
+
+
+
+
